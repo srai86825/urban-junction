2719e4fe8f4855f478ca4618466fc6af15ebf374
saurabh kumar rai <srai86825@gmail.com>
2023-07-18 23:44:51 +0530

    removed env part from sanity

--- sanity.cli.js
+++ sanity.cli.js
@@ -4,7 +4,7 @@
 **/
 import { defineCliConfig } from 'sanity/cli'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+const projectId = "h81e0hka"
+const dataset = "production"
 
 export default defineCliConfig({ api: { projectId, dataset } })
